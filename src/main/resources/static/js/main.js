a66ff46fa9310b38b01fa4839e7765e5ae26aaf7
WANKYU SO <121412330+thdhksrb@users.noreply.github.com>
2023-06-18 23:30:09 +0900

    js 수정

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -15,7 +15,7 @@ document.getElementById("getAccounts").addEventListener("click", function() {
             console.log(data);
         },
         error: function(xhr, status, error) {
-            // 에러 처리 코드 작성
+            
             console.error(error);
         }
     });
